5e3d52a0832aa5076f6eec5a46e62f40287054c1
zackdcat <158817447+Zacklikescats@users.noreply.github.com>
2025-12-07 15:09:38 +0000

    Fix command string formatting in ffmpeg.py

--- ffmpeg.py
+++ ffmpeg.py
@@ -7,7 +7,7 @@ SI = input("> ")
 print("Enter the place where you want the file to end up.")
 TI = input("> ")
 
-command = f'ffmpeg -i "{SI}{I}" -vf "scale=1280:-2,fps=30" -c:v libx264 -preset veryfast -profile:v baseline -level 3.0 -crf 28 -c:a aac -b:a 96k -movflags +faststart "{TI}{I}.mp4"'
+command = f'ffmpeg -i "+{SI}+"/"+{I}+" -vf "scale=1280:-2,fps=30" -c:v libx264 -preset veryfast -profile:v baseline -level 3.0 -crf 28 -c:a aac -b:a 96k -movflags +faststart "{TI}{I}.mp4"'
 
 print("This is your command:")
 print(command)
