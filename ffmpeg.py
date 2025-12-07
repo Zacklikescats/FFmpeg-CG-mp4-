1b1382be5f749f524bd38ac97c6e043596a795bd
zackdcat <158817447+Zacklikescats@users.noreply.github.com>
2025-12-07 15:15:37 +0000

    Modify ffmpeg.py to include FPS input and variable

--- ffmpeg.py
+++ ffmpeg.py
@@ -1,13 +1,15 @@
 import subprocess
 
 print("Enter file name:")
-I = input("> ")
+DRCRTY = input("> ")
 print("Directory of file:")
 SI = input("> ")
-print("Enter the place where you want the file to end up.")
+print("Enter the place where you want the file to end up:")
 TI = input("> ")
+print("Enter FPS:")
+FPS = input("> ")
 
-command = f'ffmpeg -i {SI}/{I} -vf "scale=1280:-2,fps=30" -c:v libx264 -preset veryfast -profile:v baseline -level 3.0 -crf 28 -c:a aac -b:a 96k -movflags +faststart "{TI}/{I}.mp4"'
+command = f'ffmpeg -i {SI}/{DRCRTY} -vf "scale=1280:-2,fps={FPS}" -c:v libx264 -preset veryfast -profile:v baseline -level 3.0 -crf 28 -c:a aac -b:a 96k -movflags +faststart "{TI}/{I}.mp4"'
 
 print("This is your command:")
 print(command)
