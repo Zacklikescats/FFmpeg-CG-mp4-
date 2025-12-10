876c6bc90f6eea07d406189b23e326b3b0fefd7b
zackdcat <158817447+Zacklikescats@users.noreply.github.com>
2025-12-10 17:47:28 +0000

    Fix output file name in ffmpeg command

--- ffmpeg.py
+++ ffmpeg.py
@@ -9,7 +9,7 @@ TI = input("> ")
 print("Enter FPS:")
 FPS = input("> ")
 
-command = f'ffmpeg -i {SI}/{DRCRTY} -vf "scale=1280:-2,fps={FPS}" -c:v libx264 -preset veryfast -profile:v baseline -level 3.0 -crf 28 -c:a aac -b:a 96k -movflags +faststart "{TI}/{I}.mp4"'
+command = f'ffmpeg -i {SI}/{DRCRTY} -vf "scale=1280:-2,fps={FPS}" -c:v libx264 -preset veryfast -profile:v baseline -level 3.0 -crf 28 -c:a aac -b:a 96k -movflags +faststart "{TI}/{SI}.mp4"'
 
 print("This is your command:")
 print(command)
